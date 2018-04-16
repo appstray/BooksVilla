b3b12e54bea2556c680621eb81afa0a0ddbfdeee
Abhishek Chaudhary <kumarakc55@gmail.com>
2018-04-16 11:01:02 +0530

    Initial Commit

---
+++ BooksVilla/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BooksVilla
+//
+//  Created by DataNova on 26/01/1940 Saka.
+//  Copyright © 1940 Appstray. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
